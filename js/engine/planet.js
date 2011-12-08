a1631ac32c0092bfa01a38c933fad9db70996988
Jefferson Carpenter <jcarpenter2@wisc.edu>
2011-12-08 14:54:46 -0600

    Planets orbit the right way again.  Oops.

--- js/engine/planet.js
+++ js/engine/planet.js
@@ -64,13 +64,13 @@ var Planet = function(pTexture, aTexture, x, y, z, radius, orbitRadius, orbitAro
 	this.planetModel = new Model("ball.obj", pTexture);
 	this.planetModel.setPosition(x,y,z);
 	this.planetModel.setScale(radius,radius,radius);
-	this.planetModel.constantRotation(0,-0.4,0);
+	this.planetModel.constantRotation(0,0,0.4);
 	if (aTexture != null) {
 		this.hasAtmos = true;
 		this.atmosModel = new Model("ball.obj", aTexture);
 		this.atmosModel.setPosition(this.x,this.y,this.z);
 		this.atmosModel.setScale(radius * 1.1, radius * 1.1, radius * 1.1);
-		this.atmosModel.constantRotation(0,-0.7,0);
+		this.atmosModel.constantRotation(0,0,0.7);
 	}
 
     /// do init a game object before using it
