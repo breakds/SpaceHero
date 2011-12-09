849971cd67c796fabb5367a89c952b223b367276
Yiqing Yang <breakds@cs.wisc.edu>
2011-12-08 21:21:39 -0600

    BattleMap Hexagons Updated

--- js/component/BattleMap.js
+++ js/component/BattleMap.js
@@ -1,139 +1,95 @@
 var BattleHexagonView = function( m, radius ) {
     this.setModel( m );
     this.register( battlefield );
-
-    
     this.radius = radius;
 
 
-
+    this.sx = -6.0;
+    this.sy = -2.7;
     
     
 
-    this.hexagonVertexBuffer = new Array();
-    
+    this.hexagonVertexBuffers = new Array();
+
+
+    /// Init ColorBuffer
+    var colors = new Array();
+    for ( var i=0; i<24; i++ ) {
+	colors.push(1.0);
+    }
+    this.colorBuffer = gl.createBuffer();
+    this.colorBuffer.itemSize = 4;
+    this.colorBuffer.numItems = 6;
+    gl.bindBuffer( gl.ARRAY_BUFFER, this.colorBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW );
+
     
     this.addHexagon = function( x, y ) {
-	
-    }
-
-
-    
-    this.drawHexagon = function( x, y ) {
-	var r = 0.5;
+	var r = this.radius;
 	var ang = 0.0;
 	var step = Math.PI / 3.0;
-	
-
-	
-	var vertices = gl.createBuffer();
-
-
-
 	var vertices = new Array();
-	vertices.push( x + r );
-	vertices.push( y );
-	vertices.push( -20 );
-	for ( var i=0; i<5; i++ )
+	for ( var i=0; i<6; i++ )
 	{
 	    ang += step;
 	    vertices.push( x + r * Math.cos( ang ) );
 	    vertices.push( y + r * Math.sin( ang ) );
-	    vertices.push( -20 );
+	    vertices.push( -7 );
 	}
-
-	
-	var colors = new Array();
-	for ( var i=0; i<24; i++ ) {
-	    colors.push(1.0);
-	}
-
-
 	var vertexBuffer = gl.createBuffer();
 	vertexBuffer.itemSize = 3;
 	vertexBuffer.numItems = 6;
 	gl.bindBuffer( gl.ARRAY_BUFFER, vertexBuffer );
 	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW );
-	gl.vertexAttribPointer( notShaderProgram.vertexPositionAttribute,
-				vertexBuffer.itemSize,
-				gl.FLOAT,
-				false,
-				0, 0 );
-	
-	
-	var colorBuffer = gl.createBuffer();
-	colorBuffer.itemSize = 4;
-	colorBuffer.numItems = 6;
-	gl.bindBuffer( gl.ARRAY_BUFFER, colorBuffer );
-	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW );
-	gl.vertexAttribPointer( notShaderProgram.vertexColorAttribute,
-				colorBuffer.itemSize,
-				gl.FLOAT,
-				false,
-				0, 0 );
-
-
-
-	setMatrixUniforms( notShaderProgram );
-	
-	
-	gl.drawArrays( gl.LINE_LOOP, 0, vertexBuffer.numItems );
-    }
-    this.draw = function() {
-	/*
-	cam.position[0] = 0.0;
-	cam.position[1] = 0.0;
-	cam.position[2] = 0.0;
-	*/
-
-
-	setShader( notShaderProgram );
-	this.drawHexagon( 0.0, 0.0 );
-	setShader( lightShaderProgram );
+	this.hexagonVertexBuffers.push( vertexBuffer );
     }
 
-    
     /*
-    var resolution = 100.0;
-    
-    var vertices = new Array();
-    var colors = new Array();
-    
-    for(var i = 0.0; i < resolution; i++)
-    {
-	vertices.push(10 * Math.sin(2.0 * Math.PI * i / resolution));
-	vertices.push(10 * Math.cos(2.0 * Math.PI * i / resolution));
-	vertices.push(0);
-	colors.push(1.0);
-	colors.push(1.0);
-	colors.push(1.0);
-	colors.push(0.5);
-    }
-    
-    this.circleVertexBuffer = gl.createBuffer();
-    this.circleColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.circleVertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.circleColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    this.circleVertexBuffer.itemSize = 3;
-    this.circleVertexBuffer.numItems = resolution;
-    this.circleColorBuffer.itemSize = 4;
-    this.circleColorBuffer.numItems = resolution;
-    
-    this.draw = function()
-    {
-	setShader(notShaderProgram);
-	gl.bindBuffer(gl.ARRAY_BUFFER, this.circleVertexBuffer);
-	gl.vertexAttribPointer(notShaderProgram.vertexPositionAttribute, this.circleVertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	gl.bindBuffer(gl.ARRAY_BUFFER, this.circleColorBuffer);
-	gl.vertexAttribPointer(notShaderProgram.vertexColorAttribute, this.circleColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	setMatrixUniforms(notShaderProgram);
-	gl.drawArrays(gl.LINE_LOOP, 0, this.circleVertexBuffer.numItems);
-	setShader(lightShaderProgram);
+    this.initHexagons = function() {
+
+	
     }
     */
+    this.initHexagons = function() {
+	var x = this.sx;
+	var y = this.sy;
+	var smallerRadius = this.radius * Math.sqrt(3) * 0.5;
+	for ( var u=0; u<this.model.cols; u++ ) {
+	    y = this.sy + this.model.lower[u] * smallerRadius;
+	    for ( var v=this.model.lower[u]; v<=this.model.upper[u]; v+=2 )
+	    {
+		this.addHexagon( x, y );
+		y += smallerRadius * 2;
+	    }
+	    x += this.radius * 1.5;
+	}
 
+    }
+    this.initHexagons();
+
+    
+    
+    this.draw = function() {
+	setShader( notShaderProgram );
+	for ( var i=0; i<this.hexagonVertexBuffers.length; i++ ) {
+	    gl.bindBuffer( gl.ARRAY_BUFFER, this.hexagonVertexBuffers[i] );
+	    gl.vertexAttribPointer( notShaderProgram.vertexPositionAttribute,
+				    this.hexagonVertexBuffers[i].itemSize,
+				    gl.FLOAT,
+				    false,
+				    0, 0 );
+	    gl.bindBuffer( gl.ARRAY_BUFFER, this.colorBuffer );
+	    gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW );
+	    gl.vertexAttribPointer( notShaderProgram.vertexColorAttribute,
+				    this.colorBuffer.itemSize,
+				    gl.FLOAT,
+				    false,
+				    0, 0 );
+	    setMatrixUniforms( notShaderProgram );
+	    gl.drawArrays( gl.LINE_LOOP, 0, this.hexagonVertexBuffers[i].numItems );
+	}
+	setShader( lightShaderProgram );
+    }
 }
 
 BattleHexagonView.prototype = new View;
--- js/prototype.js
+++ js/prototype.js
@@ -20,8 +20,8 @@ univMap.addSolarSystem( 2, 3);
 
 
 /// Create Battle Map
-var batMap = new HexagonMap( 10,10 );
-var batMapView = new BattleHexagonView( batMap, 0.2 );
+var batMap = new HexagonMap( 15,11 );
+var batMapView = new BattleHexagonView( batMap, 0.25 );
 
 
 
@@ -36,6 +36,7 @@ game.play();
 dispatcher.broadcast( { name: "StartBattle", 
 			commander0: forces[0].commanders[0],
 			commadner1: forces[1].commanders[0] } );
+
 
 
 
