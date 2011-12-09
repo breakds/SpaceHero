8c137e2e5dc754db1e9183238ae4bc2f60744ed0
Yiqing Yang <breakds@cs.wisc.edu>
2011-12-09 13:36:53 -0600

    quantity changed

--- js/prototype.js
+++ js/prototype.js
@@ -28,16 +28,21 @@ var batMapView = new BattleHexagonView( batMap, 30 );
 /// Player Commander
 forces[0].createCommander( "General", "Lionheart", 2, 5 );
 forces[0].commanders[0].addUnit( Warship );
+forces[0].commanders[0].units[0].setQuantity( 4 );
 forces[0].commanders[0].addUnit( Gunboat );
-forces[0].commanders[0].units[1].setQuantity( 10 );
+forces[0].commanders[0].units[1].setQuantity( 20 );
 forces[0].commanders[0].addUnit( Cruiser );
+forces[0].commanders[0].units[2].setQuantity( 3 );
 forces[0].commanders[0].addUnit( Sniper );
+forces[0].commanders[0].units[3].setQuantity( 5 );
 
 
 forces[1].createCommander( "General", "Luther", 7, 10 );
 forces[1].commanders[0].addUnit( Fighter );
+forces[1].commanders[0].units[0].setQuantity( 50 );
 forces[1].commanders[0].addUnit( Warrior );
 forces[1].commanders[0].addUnit( Sniper );
+forces[1].commanders[0].units[2].setQuantity( 3 );
 
 
 var planetMenu = new PlanetMenu();
